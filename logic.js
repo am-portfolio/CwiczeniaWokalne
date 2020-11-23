e2ebd30a4e081f46033cedf5ff6510e5448696b4
Aron <aron.mandrella@gmail.com>
2020-11-23 11:36:40 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -814,7 +814,7 @@ $(document).ready(function() {
 		application();
 		
 		//#TODO: Sekcja pomocy
-		$('*[href="#help"').click(function(){
+		$('a[href=#help').click(function(){
 			showToast({
 				theme: 'warning',
 				title: 'TODO',
